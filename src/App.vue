53bbbad8d259e472ce6d138dc11ffa7573e55266
Pavlova Marina <marizza.mil89@gmail.com>
2021-11-12 15:29:19 +0200

    03_Lifecycle_hooks

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,21 @@
 <template>
-  <h1>Ninja Reaction Timer</h1>
-  <button @click="start" :disabled="isPlaying">play</button>
-  <Block v-if="isPlaying" :delay="delay" />
+  <h1>My Reaction Timer</h1>
+
+  <button @click="start" :disabled = "isPlaying" >Play</button>
+
+  <!-- <Block v-if="isPlaying" :delay = "delay" /> -->
+  
 </template>
 
 <script>
-import Block from './components/Block'
+
+import Block from './components/Block.vue'
+
 export default {
   name: 'App',
+
   components: { Block },
+
   data() {
     return {
       isPlaying: false,
@@ -17,9 +24,9 @@ export default {
   },
   methods: {
     start() {
-      // set time amount (ms)
       this.delay = 2000 + Math.random() * 5000
       this.isPlaying = true
+      console.log(this.delay)
     },
   }
 }
@@ -33,20 +40,5 @@ export default {
     text-align: center;
     color: #444;
     margin-top: 60px;
-  }
-  button {
-    background: #0faf87;
-    color: white;
-    border: none;
-    padding: 8px 16px;
-    border-radius: 4px;
-    font-size: 16px;
-    letter-spacing: 1px;
-    cursor: pointer;
-    margin: 10px;
-  }
-  button[disabled] {
-    opacity: 0.2;
-    cursor: not-allowed;
   }
 </style>
